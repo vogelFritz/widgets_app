c2f014e143f1f47da8f4d729555cc2b55eddb61b
fritz <franciscosantin@hotmail.com>
2023-12-16 18:31:04 -0300

    rutas básicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -43,6 +44,10 @@ class _CustomListTile extends StatelessWidget {
         title: Text(menuItem.title),
         subtitle: Text(menuItem.subTitle),
         leading: Icon(menuItem.icon, color: colors.primary),
-        onTap: () {});
+        onTap: () {
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(builder: (context) => const ButtonsScreen()));
+          Navigator.pushNamed(context, menuItem.link);
+        });
   }
 }
